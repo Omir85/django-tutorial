1eb1a25fa069e2592896f31bb28ddd63a158c222
Jean-Michel Bernard <jean-michelbernard@eurofins.com>
2023-08-11 16:01:19 +0800

    Implement custom form to add todo list items

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -7,6 +7,24 @@ from .forms import CreateNewList
 def index(response, id):
     ls = ToDoList.objects.filter(id = id)
     if ls:
+        ls = ToDoList.objects.get(id = id)
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c"+str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                item.save()
+        elif response.POST.get("newItem"):
+            text = response.POST.get("new")
+
+            if len(text) > 2:
+                ls.item_set.create(text=text, complete=False)
+            else:
+                print("Invalid")
+    if ls:
         ls = ToDoList.objects.get(id=id)
         return render(response, "main/list.html", {"ls" : ls})
     else:
