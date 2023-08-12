0b06307cb89d5ff014360dfbcc095e43bd675b71
Jean-Michel Bernard <jean-michelbernard@eurofins.com>
2023-08-12 15:51:25 +0800

    Make todo lists user-specific and prevent a user to see the todolists of another user

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from . import views
+# from ..register import views as registration_views
 
 urlpatterns = [
     path("<int:id>", views.index, name="index"),
     path("", views.home, name="home"),
     path("create/", views.create, name="create"),
+    path("view/", views.view, name="view"),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,34 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from .models import ToDoList, Item
+from django.http import HttpResponseRedirect
+from .models import ToDoList
 from .forms import CreateNewList
 
 # Create your views here.
 def index(response, id):
-    ls = ToDoList.objects.filter(id = id)
-    if ls:
-        ls = ToDoList.objects.get(id = id)
-    if response.method == "POST":
-        print(response.POST)
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c"+str(item.id)) == "clicked":
-                    item.complete = True
-                else:
-                    item.complete = False
-                item.save()
-        elif response.POST.get("newItem"):
-            text = response.POST.get("new")
+    ls = ToDoList.objects.get(id = id)
+    if ls in response.user.todolist.all():
+        if response.method == "POST":
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c"+str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+                    item.save()
+            elif response.POST.get("newItem"):
+                text = response.POST.get("new")
 
-            if len(text) > 2:
-                ls.item_set.create(text=text, complete=False)
-            else:
-                print("Invalid")
-    if ls:
-        ls = ToDoList.objects.get(id=id)
+                if len(text) > 2:
+                    ls.item_set.create(text=text, complete=False)
+                else:
+                    print("Invalid")
         return render(response, "main/list.html", {"ls" : ls})
-    else:
-        return render(response, "main/base.html", {})
+    return render(response, "main/view.html", {})
 
 def home(response):
     return render(response, "main/home.html", {})
@@ -40,7 +35,11 @@ def create(response):
             name = form.cleaned_data["name"]
             t = ToDoList(name=name)
             t.save()
+            response.user.todolist.add(t)
         return HttpResponseRedirect("/%i" %t.id)
     else:
         form = CreateNewList()
     return render(response, "main/create.html", {"form" : form})
+
+def view(response):
+    return render(response, "main/view.html", {})
