77fdc2e1f121799339efb0eb182982aa139baf51
Jean-Michel Bernard <jean-michelbernard@eurofins.com>
2023-08-12 13:32:13 +0800

    Add user registration page and processing

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig'
+    'crispy_forms',
+    'crispy_bootstrap4',
+    'main.apps.MainConfig',
+    'register.apps.RegisterConfig'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
